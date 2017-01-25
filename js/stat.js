891e93fbab91d3c65a2bd1331771193c7abcfb4b
igorbriukhovetskii <igorbruhoveckij@gmail.com>
2017-01-25 21:25:33 +0300

    Разбил код на функции, добавил переменные

--- js/stat.js
+++ js/stat.js
@@ -1,18 +1,37 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  //  Отрисовка тени поля с результатами
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+  //  Функция отрисовки пустого поля для статистики
+  function drawStatisticsField() {
+    //  Координата Х левого-верхнего угла поля статистики
+    var topLeftX = 100;
+    //  Координата Y левого-верхнего угла поля статистики
+    var topLeftY = 10;
+    //  Ширина поля статистики
+    var statisticsFieldWidth = 420;
+    //  Высота поля статистики
+    var statisticsFieldHeight = 270;
+    //  Отрисовка тени поля с результатами
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(topLeftX + 10, topLeftY + 10, statisticsFieldWidth, statisticsFieldHeight);
 
-  //  Отрисовка поля, на котором будут выводиться результаты
-  ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
-  ctx.fillRect(100, 10, 420, 270);
+    //  Отрисовка поля, на котором будут выводиться результаты
+    ctx.fillStyle = 'rgba(255, 255, 255 , 1)';
+    ctx.fillRect(topLeftX, topLeftY, statisticsFieldWidth, statisticsFieldHeight);
+  }
 
-  //  Вывод заголовка для гистограммы с результатами
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 240, 40);
-  ctx.fillText('Список результатов:', 225, 60);
+  //  Функция отрисовки текста в заголовке поля со статистикой
+  function drawStatisticsHeader() {
+    //  Вывод заголовка для гистограммы с результатами
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', 240, 40);
+    ctx.fillText('Список результатов:', 225, 60);
+  }
+
+  //  Функция вычисления альфа-канала цвета колонки гистограммы
+  function getRandomOpacity() {
+    return Math.random() * (1 - 0.2) + 0.2;
+  }
 
   //  Общая высота гистограммы
   var histogramHeight = 150;
@@ -26,41 +45,42 @@ window.renderStatistics = function (ctx, names, times) {
   var columnInterval = 50;
 
   //  Определение максимального времени, затраченного на прохождение игры, хранимого в массиве times
-  var maxTime = -1;
-  for (var i = 0; i < times.length; i++) {
-    if (times[i] > maxTime) {
-      maxTime = times[i];
-    }
-  }
+  var maxTime = Math.max.apply(null, times);
 
   //  Вычисление масштаба гистограммы
   var step = maxTime / histogramHeight;
 
-  //  Цикл отрисовки колонок гистораммы, имён игроков и затраченного на прохождение игры времени
-  for (var j = 0; j < times.length; j++) {
-    //  Вычисление высоты колонки гистограммы
-    var columnHeight = times[j] / step;
-    //  Вычисление отступа по оси Х от левого края для текущей колонки гистограммы
-    var indentX = histogramFieldIndentX + (columnWidth + columnInterval) * j;
-    //  Вычисление альфа-канала цвета колонки гистограммы
-    var opacity = Math.random();
-    //  Добавляем непрозрачность, если случайное значение слишком мало, чтобы избежать генерации прозрачных столбцов
-    if (opacity <= 0.3) {
-      opacity += 0.2;
+  //  Функция отрисовки гистограммы
+  function drawHistogram() {
+    //  Цикл отрисовки колонок гистораммы, имён игроков и затраченного на прохождение игры времени
+    for (var j = 0, arrayLength = times.length; j < arrayLength; j++) {
+      var time = times[j];
+      var name = names[j];
+      //  Вычисление высоты колонки гистограммы
+      var columnHeight = time / step;
+      //  Вычисление отступа по оси Х от левого края для текущей колонки гистограммы
+      var indentX = histogramFieldIndentX + (columnWidth + columnInterval) * j;
+      //  Вычисление цвета колонки гистограммы
+      if (name === 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomOpacity() + ')';
+      }
+      //  Отрисовка колонки
+      ctx.fillRect(indentX, histogramFieldIndentY + (histogramHeight - columnHeight), columnWidth, columnHeight);
+      //  Определение цвета текста для имени игрока и отображаемой величины затраченного времени
+      ctx.fillStyle = '#000';
+      //  Отрисовка имени игрока под соответствующей колонкой гистограммы
+      ctx.fillText(name, indentX, histogramFieldIndentY + histogramHeight + 20);
+      //  Отрисовка величины затраченного времени над соответствующей колонкой гистограммы
+      ctx.fillText(Math.floor(time), indentX, histogramFieldIndentY + histogramHeight - columnHeight - 5);
     }
-    //  Вычисление цвета колонки гистограммы
-    if (names[j] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + opacity + ')';
-    }
-    //  Отрисовка колонки
-    ctx.fillRect(indentX, histogramFieldIndentY + (histogramHeight - columnHeight), columnWidth, columnHeight);
-    //  Определение цвета текста для имени игрока и отображаемой величины затраченного времени
-    ctx.fillStyle = '#000';
-    //  Отрисовка имени игрока под соответствующей колонкой гистограммы
-    ctx.fillText(names[j], indentX, histogramFieldIndentY + histogramHeight + 20);
-    //  Отрисовка величины затраченного времени над соответствующей колонкой гистограммы
-    ctx.fillText(Math.floor(times[j]), indentX, histogramFieldIndentY + histogramHeight - columnHeight - 5);
   }
+
+  //  Рисуем поле для статистики
+  drawStatisticsField();
+  //  Рисуем заголовок для статистики
+  drawStatisticsHeader();
+  //  Рисуем гистограмму
+  drawHistogram();
 };
